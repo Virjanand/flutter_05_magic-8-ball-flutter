745be47affdc3f327a797c22e3b8505c360b72e1
Virjanand <virjanand@virjanand.com>
2020-01-14 05:42:26 +0100

    Setup Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    EightBall(),
+  );
+}
+
+class EightBall extends StatelessWidget {
+  const EightBall({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Magic 8 Ball'),
+          ),
+        ),
       ),
     );
+  }
+}
